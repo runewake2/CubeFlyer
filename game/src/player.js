899892b5b0a722472b66a7ce488aaaba2d8defcc
Jim Bennett <jim.bennett@microsoft.com>
2022-08-11 12:30:53 -0700

    Update player.js

--- game/src/player.js
+++ game/src/player.js
@@ -12,8 +12,8 @@ class Player extends GameObject {
         this.setupInputs();
 
         // Create the player object - a 1 unit square cube
-        let cubeOptions = {width: 1, height: 1, depth: 1};
-        this.playerMesh = BABYLON.MeshBuilder.CreateBox("bird", cubeOptions, scene);
+        let boxOptions = {width: 1, height: 1, depth: 1};
+        this.playerMesh = BABYLON.MeshBuilder.CreateBox("bird", boxOptions, scene);
     }
 
     onDestroy() {
