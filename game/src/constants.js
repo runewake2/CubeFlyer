bbc95aea692fbb351ae97bd8d311ca82bd1ea0d3
dtmamani28 <dtmamani28@students.highline.edu>
2022-08-13 14:00:49 -0700

    fixinf

--- game/src/constants.js
+++ game/src/constants.js
@@ -7,4 +7,8 @@ const greetings = [
     '1000% more Cube!',
     'Hello, this is a greeting, please be greeted',
     'A Whole New Dimension!',
+    'Dragon and Dun'
+    'Are These Voxels?',
+    'Cubey Bird',
+    'Hello Open Source!'
 ];
