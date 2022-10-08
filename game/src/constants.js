a7113b9efc9e15a7818a0db9d06d4d967717bbf2
Satinder Singh <92389558+Rednitas10@users.noreply.github.com>
2022-10-08 13:44:00 -0700

    Update constants.js

--- game/src/constants.js
+++ game/src/constants.js
@@ -6,6 +6,6 @@ const greetings = [
     'It\'s a plane!',
     '1000% more Cube!',
     'A Whole New Dimension!',
-    'Is it a bird?',
+    'Maybe if it was more aerodynamic it could\'ve gone further...',
     'Is that the best you can do?'
 ];
