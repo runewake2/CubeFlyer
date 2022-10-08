1094f8b09f113e66698808c2838c3909accda8fb
DYLAN ULLRICH <ME@DYLANULLRICH.COM>
2022-10-08 14:22:14 -0700

    includesgroup changes

--- game/src/constants.js
+++ game/src/constants.js
@@ -2,12 +2,13 @@ const gravity = new BABYLON.Vector2(0, -9.8);
 const flightForce = 5; // The amount of force applied when the "fly" button is pressed
 
 const greetings = [
-    'It\'s a bird!',
-    'It\'s a plane!',
-    '1000% more Cube!',
-    'A Whole New Dimension!',
-    'Hello, Workshop!'
-    'Hello here!',
-    'Beautiful World',
-    'From local pc'
+  "It's a bird!",
+  "It's a plane!",
+  '1000% more Cube!',
+  'A Whole New Dimension!',
+  'Hello, Workshop!',
+  'Hello here!',
+  'Beautiful World',
+  'From local pc',
+  'Follow @dylanullrich on GH',
 ];
