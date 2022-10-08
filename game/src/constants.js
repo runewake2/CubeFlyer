83477ac6c1f789f9e4dbb57a2f472b689874c317
Eric Scheib <90006655+EDScheib78@users.noreply.github.com>
2022-10-08 14:10:28 -0700

    Update constants.js

--- game/src/constants.js
+++ game/src/constants.js
@@ -6,10 +6,7 @@ const greetings = [
     'It\'s a plane!',
     '1000% more Cube!',
     'A Whole New Dimension!',
-<<<<<<< HEAD
-    'Hello Dolly'
-=======
+    'Hello Dolly',
     'Hello, Open Source!',
     'Totally cubular man!'
->>>>>>> 121f74b42a38e4bb5541e7f05a9a7a89a5f5a4eb
 ];
